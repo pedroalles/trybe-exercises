682cc3678cbab343684e25c6b2b78b54f3c6493b
pedroalles <pedroalles@gmail.com>
2021-06-23 15:31:57 -0300

    Realizado exercicio 1.

---
+++ exercises/INTRODUCTION/BLOCO_08/DIA_01/ex01.js
@@ -0,0 +1,16 @@
+const employeeGen = (fullName) => {
+  const formatedName = fullName.toLowerCase().split(' ').join('_');
+  const email = `${formatedName}@trybe.com`;
+  return { fullName, email }
+};
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  };
+  return employees;
+};
+
+console.log(newEmployees(employeeGen));
