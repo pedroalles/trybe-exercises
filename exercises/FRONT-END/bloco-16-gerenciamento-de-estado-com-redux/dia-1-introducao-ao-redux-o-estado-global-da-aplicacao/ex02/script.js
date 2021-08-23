4176c63d93e792f4645e1568519cd877c856b960
pedroalles <pedroalles@gmail.com>
2021-08-23 20:30:03 -0300

    Realizado ex2

--- exercises/FRONT-END/bloco-16-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/ex02/script.js
+++ exercises/FRONT-END/bloco-16-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/ex02/script.js
@@ -12,8 +12,22 @@ const ESTADO_INICIAL_2 = {
   cidade: 'São Paulo',
 };
 
+const CHANGE_NAME = 'CHANGE_NAME';
+const CHANGE_LASTNAME = 'CHANGE_LASTNAME';
+
+const changeName = (nome) => ({ type: CHANGE_NAME, nome })
+const changeLastname = (sobrenome) => ({ type: CHANGE_LASTNAME, sobrenome })
+
 const meuPrimeiroReducer = (state = ESTADO_INICIAL_1, action) => {
   switch (action.type) {
+    case CHANGE_NAME:
+      return {
+        ...state, nome: action.nome,
+      }
+    case CHANGE_LASTNAME:
+      return {
+        ...state, sobrenome: action.sobrenome,
+      }
     default:
       return state;
   }
@@ -21,7 +35,39 @@ const meuPrimeiroReducer = (state = ESTADO_INICIAL_1, action) => {
 
 const meuSegundoReducer = (state = ESTADO_INICIAL_2, action) => {
   switch (action.type) {
+    case CHANGE_NAME:
+      return {
+        ...state, nome: action.nome,
+      }
+    case CHANGE_LASTNAME:
+      return {
+        ...state, sobrenome: action.sobrenome,
+      }
     default:
       return state;
   }
+};
+
+const reducerCombinado = Redux.combineReducers({
+  meuPrimeiroReducer,
+  meuSegundoReducer
+});
+
+const store = Redux.createStore(reducerCombinado);
+
+store.subscribe(() => {
+  const { meuPrimeiroReducer, meuSegundoReducer } = store.getState();
+
+  document.querySelector('#nome-1').textContent = meuPrimeiroReducer.nome;
+  document.querySelector('#sobrenome-1').textContent = meuPrimeiroReducer.sobrenome;
+
+  document.querySelector('#nome-2').textContent = meuSegundoReducer.nome;
+  document.querySelector('#sobrenome-2').textContent = meuSegundoReducer.sobrenome;
+});
+
+window.onload = () => {
+  setTimeout(() => {
+    store.dispatch(changeName('Pedro'))
+    store.dispatch(changeLastname('Alles'))
+  }, 1000);
 };
