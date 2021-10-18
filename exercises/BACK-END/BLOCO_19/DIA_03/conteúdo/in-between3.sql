488253d92e576828f2c1ce9ca618dfbe786ae872
pedroalles <pedroalles@gmail.com>
2021-10-18 20:47:48 -0300

    Realizados exercicios do conteudo de queries usando BETWEEN/IN do BLOCO19/DIA3

---
+++ exercises/BACK-END/BLOCO_19/DIA_03/conteúdo/in-between3.sql
@@ -0,0 +1,4 @@
+-- Descubra quantos pagamentos foram feitos entre 01/05/2005 e 01/08/2005 . Lembre-se de que, no banco de dados, as datas estão armazenadas no formato ano/mês/dia , diferente do formato brasileiro, que é dia/mês/ano
+
+SELECT COUNT(*) AS quantidade_de_pagamentos FROM payment
+WHERE DATE(payment_date) BETWEEN '2005-05-01' AND '2005-08-01';
